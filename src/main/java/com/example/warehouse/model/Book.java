7d61364fb34adcfcf06877014bd1173340be5ee1
Margadbt <margadbtt@gmail.com>
2024-11-30 14:21:29 +0800

    Login screen draft

---
+++ src/main/java/com/example/warehouse/model/Book.java
@@ -0,0 +1,16 @@
+//package com.example.warehouse.model;
+//
+//@Entity
+//public class Book {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String name;
+//    private String category;
+//    private int price;
+//    private int stock;
+//
+//    // Getters and Setters
+//}
+
