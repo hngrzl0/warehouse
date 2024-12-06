2c6d76eb9fc54982d7983bc15afbbfe56b3f6ba0
hngrzl0 <124540636+hngrzl0@users.noreply.github.com>
2024-12-07 01:30:27 +0800

    login ui

--- src/main/java/com/example/warehouse/LoginScreenController.java
+++ src/main/java/com/example/warehouse/LoginScreenController.java
@@ -3,7 +3,7 @@ package com.example.warehouse;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginScreenController {
     @FXML
     private Label welcomeText;
 
