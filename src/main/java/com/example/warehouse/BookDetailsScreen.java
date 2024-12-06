5a1cf613212a492192fb37d4f3500b60cecb1015
hngrzl0 <124540636+hngrzl0@users.noreply.github.com>
2024-12-07 00:48:30 +0800

    book details screen added

--- src/main/java/com/example/warehouse/BookDetailsScreen.java
+++ src/main/java/com/example/warehouse/BookDetailsScreen.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class CartScreen extends Application {
+public class BookDetailsScreen extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CartScreen.class.getResource("layout/screen_cart.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CartScreen.class.getResource("layout/screen_book_details.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Cart Screen");
+        stage.setTitle("Book Details Screen");
         stage.setScene(scene);
         stage.show();
     }
